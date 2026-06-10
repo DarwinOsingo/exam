9b94a540f0668947c4daf5d383bc29e6819fcf93
Darwin Osingo <osingoosteen@gmail.com>
2026-06-10 21:46:45 +0300

    prep for assesment

--- example.py
+++ example.py
@@ -7,37 +7,39 @@ import re
 import sys
 
 #
-# Complete the 'twoSum' function below.
+# Complete the 'isValid' function below.
 #
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY arr
-#  2. INTEGER target
+# The function is expected to return a BOOLEAN.
+# The function accepts STRING s as parameter.
 #
 
-def twoSum(arr, target):
-    seen = {}
-    for i,num in enumerate(arr):
-        complement = target - num
-        if complement in seen:
-            return [seen[complement],i]
-        seen[num]= i
-
+def isValid(s):
+    stack = []
+    comp = {
+        '(':')',
+        '[':']',
+        '{':'}'
+    }
+    for char in s:
+        if char in comp:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            last = stack.pop()
+            if comp[last] != char:
+                return False
+    return len(stack)== 0
     
 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input().strip())
+    s = input()
 
-    arr = list(map(int, input().rstrip().split()))
+    result = isValid(s)
 
-    target = int(input().strip())
-
-    result = twoSum(arr, target)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
+    fptr.write(str(result) + '\n')
 
     fptr.close()
