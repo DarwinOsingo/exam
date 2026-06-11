d564d9f081d97fab750c176eb0134472f72fe7ea
Darwin Osingo <osingoosteen@gmail.com>
2026-06-11 09:14:23 +0300

    new test

--- example.py
+++ example.py
@@ -1,4 +1,12 @@
-#!/bin/python3
+# Problem: Maximum Subarray Sum (Kadane's Algorithm)
+# Given an integer array, find the contiguous subarray with the largest sum and return that sum.
+# Example:
+# Input:  arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# Output: 6
+# ([4, -1, 2, 1] has the largest sum = 6)
+
+# HackerRank Starting Point:
+# python#!/bin/python3
 
 import math
 import os
@@ -7,38 +15,25 @@ import re
 import sys
 
 #
-# Complete the 'isValid' function below.
+# Complete the 'maxSubarray' function below.
 #
-# The function is expected to return a BOOLEAN.
-# The function accepts STRING s as parameter.
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def isValid(s):
-    stack = []
-    comp = {
-        '(':')',
-        '[':']',
-        '{':'}'
-    }
-    for char in s:
-        if char in comp:
-            stack.append(char)
-        else:
-            if not stack:
-                return False
-            last = stack.pop()
-            if comp[last] != char:
-                return False
-    return len(stack)== 0
-    
+def maxSubarray(arr):
+    # Write your code here
+    pass
 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s = input()
+    n = int(input().strip())
 
-    result = isValid(s)
+    arr = list(map(int, input().rstrip().split()))
+
+    result = maxSubarray(arr)
 
     fptr.write(str(result) + '\n')
 
