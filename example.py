8efa5d01e50c0757c989e49cf159edb9a9792222
Darwin Osingo <osingoosteen@gmail.com>
2026-06-16 11:08:06 +0300

    another qn

--- example.py
+++ example.py
@@ -1,43 +1,41 @@
+#Given a movie title, return its imdbRating. If the movie is not found return -1.
+# Input:  title = "Inception"
+# Output: 8.8
+
+# Input:  title = "xyzabc"
+# Output: -1
 {
     "page": 1,
-    "total_pages": 3,
+    "total_pages": 1,
+    "total": 1,
     "data": [
         {
-            "Title": "The Dark Knight",
-            "Year": 2008,
-            "imdbID": "tt0468569"
-        },
-        {
-            "Title": "Iron Man",
-            "Year": 2008,
-            "imdbID": "tt0371746"
+            "Title": "Inception",
+            "Year": 2010,
+            "imdbID": "tt1375666",
+            "imdbRating": 8.8
         }
     ]
 }
 import requests
 
-def getMoviesByYear(year):
-    URL = F"https://jsonmock.hackerrank.com/api/movies?Year={year}&page={page}"
-    response= requests.get(URL).json()
-    total_pages=int(response['total_pages'])+1
-    movies = []
-    for page in range(1,total_pages):
-        info = requests.get(F"https://jsonmock.hackerrank.com/api/movies?Year={year}&page={page}").json()
-        release = info['data']
-        for movie in release:
-            if year == movie['Year']:
-                 movies.append( movie['Title'])
-    return sorted(movies)
-            
+def getMovieRating(title):
+   
 
-
-            
-
-
+    url=f"https://jsonmock.hackerrank.com/api/movies/search?Title={title}&page={1}"
+    library = requests.get(url).json
+    data = library['data']
+    for key in data:
+        if title == key['Title']:
+            return key['imdbRating']
+    
+    return -1
+   
+ 
+    pass
 
 
 if __name__ == '__main__':
-    year = int(input())
-    result = getMoviesByYear(year)
-    for title in result:
-        print(title)
+    title = input()
+    result = getMovieRating(title)
+    print(result)
